e1f4d01741e74403a57a87f6e2d9151f4e5c4e4c
FearDread <ghaptonstall@gmail.com>
2025-07-20 00:04:28 -0700

    --add new background effect css and html

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -21,8 +21,12 @@ const Layout = (props) => {
 
       
       <ThemeSwitcher setTheme={setTheme} />
-      <div className="fixed bottom-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>
-      <div className="fixed top-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>
+	{/* <div className="fixed bottom-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>
+	    <div className="fixed top-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>*/}
+	<div className="ellipsel-bg">
+	    <div className="Ellipsel1"></div>
+	    <div className="Ellipsel2"></div>
+	    
       <div className="relative w-full max-w-screen-xl">
         <main className="relative h-full bg-gradient-to-tr from-omega-900 via-omega-900 to-omega-800">
 
@@ -34,6 +38,7 @@ const Layout = (props) => {
           </div>
         </main>
       </div>
+	    </div>
       <Menu />
     </div>
   )
